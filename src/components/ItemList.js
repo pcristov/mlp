1315c37e047cb4b9dc5c32882d8cd173a25c2fc5
Pedro Cristovão <pcristov@gmail.com>
2021-04-27 01:16:25 +0800

    Add selected class to tab to show underlined

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,9 +3,9 @@ import React from 'react'
 const ItemList = () => {
   return (
     <div className='item-list'>
-      <div className='item-list__text'>text</div>
-      <div className='item-list__available-items'>available</div>
-      <div className='item-list__information-one'>info one</div>
+      <div className='item-list__text'>Lorem ipsum dummy text. Lorem isum not just dummy text</div>
+      <div className='item-list__available-items item-list--selected'>Available Items</div>
+      <div className='item-list__information-one'>Information One</div>
       <div className='item-list__images'>images</div>
     </div>
   )

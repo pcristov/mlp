89666ce5ee5483e0ee24b3480cf7a2034a2eaaa5
Pedro Cristovão <pcristov@gmail.com>
2021-04-27 18:37:44 +0800

    Integrate Item components in ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,12 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
+import Item from './Item'
 
-const ItemList = () => {
+const ItemList = ({ images }) => {
   return (
     <div className='item-list'>
       <div className='item-list__text'>Lorem ipsum dummy text. Lorem isum not just dummy text</div>
       <div className='item-list__available-items item-list--selected'>Available Items</div>
       <div className='item-list__information-one'>Information One</div>
-      <div className='item-list__images'>images</div>
+      <div className='item-list__images'>
+        { images && images.map((image, index) => (
+          <Item item={ image } key={ index } />
+        ))
+        }
+      </div>
     </div>
   )
 }

0fea6cc00fbcf741171ef129f7a18d914581a1c1
Pedro Cristovão <pcristov@gmail.com>
2021-04-27 13:11:49 +0800

    Add header component to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect }  from 'react'
+import Header from './Header'
 import Summary from './Summary'
 import ItemList from './ItemList'
 
@@ -6,25 +7,26 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true)
 
   function fakeRequest() {
-    return new Promise(resolve => setTimeout(() => resolve(), 2500));
+    return new Promise(resolve => setTimeout(() => resolve(), 2500))
   }
 
   useEffect(() => {
     fakeRequest().then(() => {
-      const el = document.querySelector('.loader-container');
+      const el = document.querySelector('.loader-container')
       if (el) {
-        el.remove();
-        setIsLoading(!isLoading);
+        el.remove()
+        setIsLoading(!isLoading)
       }
-    });
-  }, []);
+    })
+  }, [])
 
   if (isLoading) {
-    return null;
+    return null
   }
 
   return (
     <div className='container'>
+      <Header />
       <Summary />
       <ItemList />
     </div>

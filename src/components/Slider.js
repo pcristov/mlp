c047a37ad1bd26e2d8db5e903f41e16b2ddcef6b
Pedro Cristovão <pcristov@gmail.com>
2021-05-06 15:05:47 +0800

    Add main Slider component

---
+++ src/components/Slider.js
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import SliderContent from './SliderContent'
+import Arrow from './Arrow'
+import '../slider.css'
+
+const Slider = ({ slides }) => {
+  const getWidth = () => window.innerWidth
+
+  const [state, setState] = useState({
+    activeSlide: 0,
+    translate: 0,
+    transition: 0.45
+  })
+
+  const { translate, transition, activeSlide } = state
+
+  // Function to handleClick of next arrow
+  const nextSlide = () => {
+
+    // if activeSlide is the last one
+    if (activeSlide === slides.length - 1) {
+      return setState({
+        ...state,
+        translate: 0,
+        activeSlide: 0
+      })
+    }
+
+    setState({
+      ...state,
+      activeSlide: activeSlide + 1,
+      translate: (activeSlide + 1) * getWidth()
+    })
+  }
+
+  // Function to handleClick of previous arrow
+  const prevSlide = () => {
+
+    // if activeSlide is the first one
+    if (activeSlide === 0) {
+      return setState({
+        ...state,
+        translate: (slides.length - 1) * getWidth(),
+        activeSlide: slides.length - 1
+      })
+    }
+
+    setState({
+      ...state,
+      activeSlide: activeSlide - 1,
+      translate: (activeSlide - 1) * getWidth()
+    })
+  }
+
+  return (
+    <div className='slider'>
+      <SliderContent translate={ translate } transition={ transition } width={ getWidth() * slides.length } content={ slides } />
+
+      <Arrow direction='left' handleClick={ prevSlide } />
+      <Arrow direction='right' handleClick={ nextSlide } />
+    </div>
+  )
+}
+
+export default Slider

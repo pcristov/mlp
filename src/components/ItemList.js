2cef87a687e193838507a8ea262e030964a318b1
Pedro Cristovão <pcristov@gmail.com>
2021-05-06 15:02:44 +0800

    Include Slider component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import Item from './Item'
+import Slider from './Slider'
 
 const ItemList = ({ images }) => {
   return (
@@ -8,12 +8,7 @@ const ItemList = ({ images }) => {
       <div className='item-list__text'>Lorem ipsum dummy text. Lorem isum not just dummy text</div>
       <div className='item-list__available-items item-list--selected'>Available Items</div>
       <div className='item-list__information-one'>Information One</div>
-      <div className='item-list__images'>
-        { images && images.map((image, index) => (
-          <Item item={ image } key={ index } />
-        ))
-        }
-      </div>
+      <Slider slides={ images } />
     </div>
   )
 }
